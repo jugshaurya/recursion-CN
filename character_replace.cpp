bab936ddf97b6071f3930165106f0f032ed05f24
jugshaurya <39825643+jugshaurya@users.noreply.github.com>
2018-06-03 17:00:52 +0530

    added the question as well

--- character_replace.cpp
+++ character_replace.cpp
@@ -1,3 +1,9 @@
+/*
+Q-->Given an input string S and two characters c1 and c2,
+you need to replace every occurrence of character c1 with 
+character c2 in the given string.*/
+
+
 void replaceCharacter(char input[], char c1, char c2) {  
   	if(input[0] == '\0'){
       return ;
